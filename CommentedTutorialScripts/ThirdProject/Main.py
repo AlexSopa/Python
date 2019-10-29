07b093ba1b3068b170ccc4e9b5298298c0682c79
AlexSopa <56882081+AlexSopa@users.noreply.github.com>
2019-10-29 09:55:39 -0500

    Creating a (simple)neural network from scratch
    Using only NumPy & Matplotlib.pyplot for easier calculations and easier graphing of data respectively.

---
+++ CommentedTutorialScripts/ThirdProject/Main.py
@@ -0,0 +1,6 @@
+#TO-DO 
+#Attempt to perfect knowledge of Calculus & different functions used in Machine Learning
+#Apply knowledge in python using NumPy & MatplotLib to graph & better my understanding
+#Create my own Neural network without using pre-made NN libraries(Keras)
+#Apply changes & challenge the network to get the accuracy high & false positives low
+#--Maybe do something with this & Twitter API
