b34be6b26511c4e5c8461a4d2583adb980666865
AlexSopa <56882081+AlexSopa@users.noreply.github.com>
2019-10-29 10:00:43 -0500

    Added #Plan

--- CommentedTutorialScripts/ThirdProject/Main.py
+++ CommentedTutorialScripts/ThirdProject/Main.py
@@ -1,5 +1,9 @@
-#TO-DO 
-#Attempt to perfect knowledge of Calculus & different functions used in Machine Learning
+#Plan
+#Follow https://towardsdatascience.com/machine-learning-for-beginners-an-introduction-to-neural-networks-d49f22d238f9 for guided learning
+#Use knowledge from https://www.3blue1brown.com/neural-networks 
+
+#TO-DO
+#Broaden knowledge of Calculus used in Machine Learning
 #Apply knowledge in python using NumPy & MatplotLib to graph & better my understanding
 #Create my own Neural network without using pre-made NN libraries(Keras)
 #Apply changes & challenge the network to get the accuracy high & false positives low
